131b92f7b46be4f14348e33859e590f5a5031a27
Leograzioli <leograzioli@gmail.com>
2022-10-14 15:21:16 +0200

    palindroma

---
+++ Palindroma/js/script.js
@@ -0,0 +1,26 @@
+const userImput = prompt("Tell us a name");
+
+
+//to know if the inputed word by the client is palindrome or not
+//userImput -> string 
+//retun -> bolean
+function isPalindroma(name) {
+    let reverseName = "";
+    for (let i = (name.length -1); i >= 0 ; i--) {
+        const element = name[i];
+        reverseName += element;
+        console.log(reverseName);
+    }
+
+    let isEqual = false;
+    if (reverseName === name) {
+        isEqual = true;
+        alert("it is palindrome");
+    } else {
+    console.log(isEqual);
+    alert("is not palindrome");
+    }
+    return isEqual;
+}
+
+isPalindroma(userImput);
